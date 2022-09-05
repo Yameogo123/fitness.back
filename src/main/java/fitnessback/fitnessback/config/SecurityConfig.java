36b611ea865fdb347078c01b152a7b005404a809
Yameogo123 <58187516+Yameogo123@users.noreply.github.com>
2022-09-05 23:28:09 +0000

    Update SecurityConfig.java

--- src/main/java/fitnessback/fitnessback/config/SecurityConfig.java
+++ src/main/java/fitnessback/fitnessback/config/SecurityConfig.java
@@ -39,13 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                     .authorizeRequests()
-                    .antMatchers("/login/**", "/logout").permitAll()
-                .and()
-                    .authorizeRequests()
-                    .antMatchers(GET, "/user/*", "/member/*", "/contact/*").authenticated()
-                    .antMatchers(PUT, "/user/*", "/member/*", "/contact/*").authenticated()
-                    .antMatchers(POST, "/user/*", "/member/*", "/contact/*").authenticated()
-                    .antMatchers(DELETE, "/user/*").hasAnyAuthority("ADMIN", "ARTIST");
+                    .antMatchers("/login/**", "/logout", "/user/*", "/member/*", "/contact/*").permitAll();
         http
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
